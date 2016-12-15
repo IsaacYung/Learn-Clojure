487d7f17d708e5666d13c9ce3a41ce4a7bedf847
Isaac Yung Lopes <isaaczoi@gmail.com>
2016-12-15 20:21:53 -0200

    Functional forca game

--- forca/src/forca/core.clj
+++ forca/src/forca/core.clj
@@ -1,7 +1,34 @@
 (ns forca.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(def total-de-vidas 6)
+
+(defn perdeu [] (print "Você perdeu"))
+(defn ganhou [] (print "Você ganhou!"))
+
+(defn letras-faltantes [palavra acertos]
+  (remove (fn [letra] (contains? acertos (str letra))) palavra))
+
+(defn acertou-a-palavra-toda? [palavra acertos]
+  (empty? (letras-faltantes palavra acertos)))
+
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+(defn avalia-chute [chute vidas palavra acertos]
+  (if (acertou? chute palavra)
+    (jogo vidas palavra (conj acertos chute))
+    (jogo (dec vidas) palavra acertos)
+  )
+)
+
+(defn jogo [vidas palavra acertos]
+  (if (= vidas 0)
+    (perdeu)
+    (if (acertou-a-palavra-toda? palavra acertos)
+      (ganhou)
+      (avalia-chute (le-letra!) vidas palavra acertos)
+    )
+  )
+)

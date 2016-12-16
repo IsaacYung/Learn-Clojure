e38906d8a7093073cd079af6fdc6481ab59e480d
Isaac Yung Lopes <isaaczoi@gmail.com>
2016-12-16 00:26:21 -0200

    Add finished forca

--- forca/src/forca/core.clj
+++ forca/src/forca/core.clj
@@ -16,19 +16,25 @@
 
 (defn acertou? [chute palavra] (.contains palavra chute))
 
-(defn avalia-chute [chute vidas palavra acertos]
-  (if (acertou? chute palavra)
-    (jogo vidas palavra (conj acertos chute))
-    (jogo (dec vidas) palavra acertos)
-  )
-)
+(defn imprime-forca [vidas palavra acertos]
+  (println "Vidas " vidas)
+  (doseq [letra (seq palavra)]
+    (if (contains? acertos (str letra))
+    (print letra " ")
+    (print "_" " ")))
+  (println))
 
 (defn jogo [vidas palavra acertos]
-  (if (= vidas 0)
-    (perdeu)
-    (if (acertou-a-palavra-toda? palavra acertos)
-      (ganhou)
-      (avalia-chute (le-letra!) vidas palavra acertos)
-    )
-  )
-)
+  (imprime-forca vidas palavra acertos)
+  (cond
+    (= vidas 0) (perdeu)
+    (acertou-a-palavra-toda? palavra acertos) (ganhou)
+    :else
+    (let [chute (le-letra!)]
+      (if (acertou? chute palavra)
+        (do
+          (println "Acertou a letra!")
+          (recur vidas palavra (conj acertos chute)))
+        (do
+          (println "Errou a letra! Perdeu vida!")
+          (recur (dec vidas) palavra acertos))))))
